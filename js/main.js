a73048487a1c237bee7481eef365d3d4772fdcec
Mas <mas@Mass-MacBook-Pro.local>
2019-05-02 21:41:52 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//The user clicks the next button and views the next image
+//The user clicks the back button and views the previous image
+//The image are reset or ongoing so that the carousel does not end ( it only resets)
+
+let rotation = 0;
+
+let imgs = ['img/charles.jpeg', 'img/chris.jpeg']
+
+function swap(){
+  $('section.leon img').attr('src', imgs[rotation])
+}
+
+$("#leonnext").on('click', function(){
+  rotation += 1;
+ if (rotation >= imgs.length) {
+   rotation = imgs.length
+ }
+ swap()
+})
+
+$("#leonback").on('click', function(){
+  rotation -= 1;
+  if (rotation <= 0) {
+  rotation = 0
+  }
+  swap()
+})
