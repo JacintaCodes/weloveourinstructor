95a011d20a4f8ec4fabbdd629bd86b1a9018bc09
Mas <mas@Mass-MacBook-Pro.local>
2019-05-02 23:42:05 -0400

    first commit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 let rotation = 0;
 
-let imgs = ['img/charles1.jpg', 'img/charles2.jpg', 'img/chris.jpg']
+let imgs = ['img/charles1.jpg', 'img/charles2.jpg', 'img/chris.jpg', 'img/kareem.jpg', 'img/sam.jpg']
 
 function swap(){
   $('section.leon img').attr('src', imgs[rotation])
